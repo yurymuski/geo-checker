c546ba339d707133a35d8237e77a8470dd3eed8e
yurymuski <muski.yury@gmail.com>
2022-09-04 22:30:58 +0400

    moved cloudflare based geo to separate repo

--- terraform/main.tf
+++
@@ -1,4 +0,0 @@
-# CLOUDFLARE_API_TOKEN required
-provider "cloudflare" {
-  account_id = var.cloudflare_account_id
-}
--- terraform/versions.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-    }
-  }
-}
